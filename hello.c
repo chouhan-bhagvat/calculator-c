2311c77b5cecc8705ad60f7a85fbe5f6ce7a2080
Bhagwat singh <chouhanbhagwatsingh27@gmail.com>
2024-08-21 00:00:42 +0530

    add new file
    add calculator

---
+++ hello.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+
+int main () {
+
+
+
+    char operator;            // Variable to store the operator
+    double first, second;    // Variables to store the two numbers
+
+    // Prompt the user to enter the operator
+    printf("Enter the operator (+, -, *, / ) : ");
+    scanf("%c", &operator);
+    printf("enter the two numbers one by one : ");
+    scanf("%f %lf", &first,&second);
+
+
+    switch (operator)
+    {
+    case '+' :
+        // Perform addition and display result
+        printf("%lf + %lf = %lf", first,second,(first+second));
+        break;
+        
+    case '-' :
+        // Perform subtraction and display result
+        printf("%lf - %lf = %lf", first,second,(first-second));
+        break;  
+
+    case '*' :
+        // Perform multiplication and display result
+        printf("%lf + %lf = %lf", first,second,(first*second));
+        break;
+
+    case '/' :
+        if(second!=0.0)
+
+        // Perform division if second number is not zero
+          printf("%lf / %lf = %lf", first,second,(first/second));
+        else
+          printf("Divide by zero situation.");
+
+    default:
+        // Handle invalid operator input
+        printf("%c is not valid operator",operator);
+        break;
+    }
+   
+    return 0; 
+
+}   
+    
